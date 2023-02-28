a2eb84e320cfa781f95123e65b2fb1e8f618d9bf
hanng67 <giahanng67@gmail.com>
2023-02-28 23:56:15 +0700

    - update info display text item

--- Assets/Scripts/Games/Items/Core/BaseItem.cs
+++ Assets/Scripts/Games/Items/Core/BaseItem.cs
@@ -22,8 +22,8 @@ public class BaseItem : ScriptableObject
     public string GetInfoDisplayText()
     {
         StringBuilder builder = new StringBuilder();
-
-        builder.Append($"<color={Rarity.TextColour}>{Name}</color>").AppendLine();
+        
+        builder.Append($"<size=35><color={Rarity.TextColour}>{Name}</color></size>").AppendLine();
         builder.Append($"<color={Rarity.TextColour}>{Rarity.Name}</color>").AppendLine();
         builder.Append("<color=green>").Append(Description).Append("</color>").AppendLine();
         builder.Append("Sell Price: ").Append(SellPrice).Append(" Gold");
