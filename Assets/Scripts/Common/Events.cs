1546d2e5cd2400e7a2a880ac55fa9f719c009f55
hanng67 <giahanng67@gmail.com>
2023-03-01 10:32:04 +0700

    + add events manager

---
+++ Assets/Scripts/Common/Events.cs
@@ -0,0 +1,7 @@
+using UnityEngine.Events;
+
+public class Events
+{
+    public static UnityEvent<string> EventMouseStartHoverItem = new UnityEvent<string>();
+    public static UnityEvent EventMouseEndHoverItem = new UnityEvent();
+}
